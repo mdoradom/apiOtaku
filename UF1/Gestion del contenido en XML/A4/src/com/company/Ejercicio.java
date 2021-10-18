0d6c77d55aa6965676c59fcfafd50eda17628eb0
iam47577708 <mariodoradomartinez@gmail.com>
2021-10-18 19:22:57 +0200

    asd

--- UF1/Gestion del contenido en XML/A4/src/com/company/Ejercicio.java
+++ UF1/Gestion del contenido en XML/A4/src/com/company/Ejercicio.java
@@ -1,41 +1,67 @@
 package com.company;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
 class Contact {
-    String name;
-    String phone;
-    String email;
+    public String name;
+    public String phone;
+    public String email;
+
+    public Contact() {}
+
+    public Contact(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
 }
 
 class Agenda {
-    ArrayList<Contact> list;
+    public ArrayList<Contact> list = new ArrayList<>();
 }
 
 public class Ejercicio {
+
+    static Agenda agenda;
+    static Path file = Paths.get("agenda.json");
+    static ObjectMapper objectMapper = new ObjectMapper();
+
     public static void main (String[] args) throws IOException {
 
-        /*ObjectMapper objectMapper = new ObjectMapper();
-        // leer el fichero json y cargarlo en un objeto "agenda"
-        Contact contact = objectMapper.readValue(Paths.get("agenda.json").toFile(), Contact.class);
-        Agenda agenda = new Agenda();
 
-        agenda.list = new ArrayList<>();
-        agenda.list.add(contact);
 
-        System.out.println(agenda.list);
 
-        // add, eliminar contactos
 
-        // guardar el objecto agenda en el fichero json*/
 
-        Contact contact = new Contact();
-        contact.name = "pepe";
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.writeValue(Paths.get("agenda.json").toFile(), contact);
+            // add, eliminar contactos
+
+            // guardar el objecto agenda en el fichero json*/
 
     }
-}
+
+    public void addContact(String name, String phone, String email) {
+        agenda.list.add(new Contact(name,phone,email));
+        try {
+            agenda = objectMapper.readValue(file.toFile(), Agenda.class);
+        } catch (Exception e) {
+            agenda = new Agenda();
+        }
+    }
+
+    public boolean delContact(String name) {
+        return agenda.list.remove(name);
+    }
+
+    public void listContact() {
+        System.out.println(agenda.list);
+    }
+
+    public void listEmailContact() {
+
+    }
+ }
