38f8bf98bb4a005d49c267d4c35eee13220a9468
mdoradom <44005739+mdoradom@users.noreply.github.com>
2022-02-14 17:16:30 +0100

    CREATE EXTENSION IF NOT EXISTS pgcrypto;

--- src/main/resources/db/migration/V3__usertable.sql
+++ src/main/resources/db/migration/V3__usertable.sql
@@ -9,7 +9,8 @@ CREATE TABLE usser (
 
 
 -- afegim un usuari de prova
-CREATE EXTENSION pgcrypto;
+-- CREATE EXTENSION pgcrypto;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 INSERT INTO usser (username, password) VALUES ('user', crypt('pass', gen_salt('bf')));
 INSERT INTO usser (username, password) VALUES ('user2', crypt('pass2', gen_salt('bf')));
 
